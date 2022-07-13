07e2930204db9ac785964d33fe71ced42882df0f
tyren234 <tyren234@gmail.com>
2022-07-13 21:26:16 +0200

    maximum subarray

---
+++ blind75/maximum_subarray/script.py
@@ -0,0 +1,16 @@
+from distutils.log import error
+
+
+def maximum_subarray(arr : list[int]) -> int:
+    if len(arr) == 0: return -1
+    sum = 0
+    cmax = arr[0]
+    for i in range(len(arr)):
+        sum += arr[i]
+        if sum > cmax:
+            cmax = sum
+        if sum < 0:
+            sum = 0
+    return cmax
+            
+print(maximum_subarray([-2,1,-3,4,-1,2,1,-5,4]))
