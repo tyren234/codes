91afcd7e898d055da65180fd14a663880176cc05
tyren234 <tyren234@gmail.com>
2022-07-13 22:02:29 +0200

    maximum product subarray not working yet

---
+++ blind75/maximum_product_subarray/script.py
@@ -0,0 +1,17 @@
+def maximum_product_subarray(arr : list[int]) -> int:
+    if len(arr) == 0:return 0
+    cmax = 1
+    cmin = 1
+    current = max(arr)
+    for i in range(len(arr)):
+        tmp = arr[i] * cmax
+        cmax = max(cmax * arr[i], cmin * arr[i], arr[i])
+        cmin = min(tmp, cmin * arr[i], arr[i])
+        current = max(current, cmax)
+
+    return cmax
+
+print(maximum_product_subarray([1,2,3]))
+print(maximum_product_subarray([-1,-2,-3]))
+print(maximum_product_subarray([-1,-2,-3,-4, 0, 15, 3]))
+
