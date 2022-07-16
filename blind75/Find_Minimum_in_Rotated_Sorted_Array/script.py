c806689019e4a4ed98244e77506e3ba2c5a9a370
tyren234 <tyren234@gmail.com>
2022-07-16 10:15:54 +0200

    find minimum in rotated sorted array done

--- blind75/Find_Minimum_in_Rotated_Sorted_Array/script.py
+++ blind75/Find_Minimum_in_Rotated_Sorted_Array/script.py
@@ -1,17 +1,47 @@
 def min_in_rotated_sorted_array (arr : list[int]) -> int:
-    if len(arr) == 0: return None
-    m = len(arr)//2
-    if arr[m] >= arr[0]:
-        # look right
-        while True:
-            if m >= len(arr)-1: return arr[0]
-            elif arr[m] > arr[m+1]: return arr[m+1]
-            else: m += 1
-    else:
-        # look left
-        while True:
-            if m <= 0: return arr[0]
-            elif arr[m] < arr[m-1]: return arr[m-1]
-            else: m -= 1
+    # res = arr[0]
+    # l = 0
+    # r = len(arr) - 1
 
-# print(min_in_rotated_sorted_array([2,5,8,11,13,57,9458,10000]))
+    # while l<=r:
+    #     if arr[l] < arr[r]:
+    #         res = min(res, arr[l])
+    #         break
+        
+    #     m = (l+r)//2
+    #     res = min(res,arr[m])
+    #     if arr[m] >= arr[l]:
+    #         l = m + 1
+    #     else: 
+    #         r = m - 1
+        
+    # return res
+
+    l = 0
+    r = len(arr) - 1
+    res = arr[0]
+
+    while l <= r:
+        if arr[l] <= arr[r]:
+            res = min(res, arr[l])
+            break
+        
+        m = (l+r)//2
+        res = min(res, arr[m])
+        if arr[m] < arr[l]:
+            r = m - 1
+        else:
+            l = m + 1
+    return res
+
+# 0,1,2,3,4,5,6,7
+# 7,0,1,2,3,4,5,6
+# 6,7,0,1,2,3,4,5
+# 5,6,7,0,1,2,3,4
+# 4,5,6,7,0,1,2,3
+# 3,4,5,6,7,0,1,2
+# 2,3,4,5,6,7,0,1
+# 1,2,3,4,5,6,7,0
+# 0,1,2,3,4,5,6,7
+
+print(min_in_rotated_sorted_array([11,13,57,9458,10000,2,5,8]))
