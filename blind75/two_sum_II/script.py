6ae20d0b626f9b83a1d774599e788e288cace655
tyren234 <tyren234@gmail.com>
2022-07-17 12:57:24 +0200

    two sum II

---
+++ blind75/two_sum_II/script.py
@@ -0,0 +1,10 @@
+def two_sum_II (arr : list[int], target : int) -> tuple:
+    l = 0
+    r = len(arr) - 1
+    while l <= r:
+        if arr[l] + arr[r] == target: return (l+1, r+1)
+        elif arr[l] + arr[r] > target: r -= 1
+        else: l += 1
+    return -1
+
+# print(two_sum_II([1,3,4,5,7,11],9))
