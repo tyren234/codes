b6806ef0731dcffe8a15c38296ebf7d8c663d01d
tyren234 <39140389+tyren234@users.noreply.github.com>
2022-07-24 21:24:03 +0200

    longest increasing subsequence

--- blind75/longest_increasing_subsequence/script.py
+++ blind75/longest_increasing_subsequence/script.py
@@ -1,10 +1,15 @@
-def computeLongestIncreasingSubsequence(arr : "list[int]")->"list[int]":
-    
-    for i in range(len(arr)).reverse(): # every number
-        for j in range(len(arr)): # every number
-            pass
-
-# print(computeLongestIncreasingSubsequence([1,2,3,4,5,6,7]))
+def computeLongestIncreasingSubsequence(arr : "list[int]")->int:
+    sizes = [0]*len(arr)
+    sizes[-1] = 1
+    for i in reversed(range(len(arr)-1)): # every number from the penultimate to the first one
+        cmax = 0
+        for j in reversed(range(i+1,len(arr))): # every number from the last one to the current ith
+            if (arr[j] > arr[i] and cmax < 1 + sizes[j]):
+                cmax = 1 + sizes[j]
+        sizes[i] = cmax
+    return max(sizes)
+                
+print(computeLongestIncreasingSubsequence([0,1,0,3,2,3]))
 # print(computeLongestIncreasingSubsequence([0,1,2,5,6,7]))
 # print(computeLongestIncreasingSubsequence([2,2,2,2,2,2,1]))
 # print(computeLongestIncreasingSubsequence([0,2,1,3,1,4,1,5]))
