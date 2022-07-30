1f205d644e1c7c73d7dbd5bff3bfaed94c4fee2b
tyren234 <39140389+tyren234@users.noreply.github.com>
2022-07-30 22:58:28 +0200

    word break

--- blind75/word_break/word_break.py
+++ blind75/word_break/word_break.py
@@ -1,2 +1,14 @@
+from regex import W
+
+
 def wordBreak(s : str, wordDict : "list[str]") -> bool:
-    pass
+    outcomes = [False] * (len(s) + 1)
+    outcomes[len(s)] = True
+
+    for i in reversed(range(len(s))):
+        for word in wordDict:
+            if i + len(word) <= len(s) and s[i:i+len(word)] == word:
+                outcomes[i] = outcomes[i+len(word)]
+            if outcomes[i] == True: # if we found matching word already, just break
+                break
+    return outcomes[0]
