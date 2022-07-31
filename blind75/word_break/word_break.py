284b0d03023bdcaa830dedfb3428f8c9f8dcf692
tyren234 <39140389+tyren234@users.noreply.github.com>
2022-07-31 14:40:52 +0200

    word break 2 not working

--- blind75/word_break/word_break.py
+++ blind75/word_break/word_break.py
@@ -1,6 +1,3 @@
-from regex import W
-
-
 def wordBreak(s : str, wordDict : "list[str]") -> bool:
     outcomes = [False] * (len(s) + 1)
     outcomes[len(s)] = True
@@ -12,3 +9,19 @@ def wordBreak(s : str, wordDict : "list[str]") -> bool:
             if outcomes[i] == True: # if we found matching word already, just break
                 break
     return outcomes[0]
+
+def wordBreak2(s : str, wordDict : "list[str]") -> bool:
+    outcomes = [False] * (len(s)+1)
+    outcomes[0] = True
+
+    # i - begining of the current word in outcomes
+    # i-1 - previous word's result in outcomes - true or false
+    # i+len(word)-1 - end of current word in outcomes
+
+    for i in range(1,len(s)+1):
+        for word in wordDict:
+            if i+len(word)-1 > len(s): continue
+            if s[i-1:i-1+len(word)] == word: outcomes[i+len(word)-1] = outcomes[i-1]
+            if outcomes[i + len(word) - 1] == True: break
+    
+    return outcomes[-1]
