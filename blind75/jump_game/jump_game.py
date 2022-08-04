5655e17e7d1a4d1cc1b68e38e4704e30ec7ae138
tyren234 <39140389+tyren234@users.noreply.github.com>
2022-08-04 12:26:39 +0200

    jump game
    has two cool solutions. There is one that doesn't require any dynamic programming only a smart little trick :)

--- blind75/jump_game/jump_game.py
+++ blind75/jump_game/jump_game.py
@@ -1,2 +1,20 @@
+# O(n^2) solution - too slow
+# def canJump(nums:"list[int]") -> bool:
+#     outcome = [False] * len(nums)
+#     outcome[-1] = True
+#     for i in reversed(range(len(nums)-1)):
+#         for k in range(1,nums[i]+1):
+#             if k+i < len(nums) and outcome[k+i]: # == True
+#                 outcome[i] = True
+#                 break
+#     return outcome[0]
+
+# O(n) solution - linear time, best option
 def canJump(nums:"list[int]") -> bool:
-    pass
+    target = len(nums) - 1
+    for i in reversed(range(len(nums)-1)):
+        if i + nums[i] >= target: target = i
+    return True if not target else False
+
+# print(canJump([2,3,1,1,4]))
+# print(canJump([3,2,1,0,4]))
