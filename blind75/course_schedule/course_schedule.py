3f5dac3f6ac06127921b1f005176b771acc27528
tyren234 <tyren234@gmail.com>
2022-08-08 22:31:33 +0200

    needs refining

--- blind75/course_schedule/course_schedule.py
+++ blind75/course_schedule/course_schedule.py
@@ -1,25 +1,28 @@
 def canFinish(numCourses : 'int', prerequisites : 'list[list[int]]') -> bool:
-    requirements = dict()
+    requirements = {x:[] for x in range(numCourses)}
     # set up requirements
     for requirement in prerequisites:
-        if requirement[0] in requirements:
-            requirements[requirement[0]].append(requirement[1])
-        else:
-            requirements[requirement[0]] = [requirement[1]]
+        requirements[requirement[0]].append(requirement[1])
 
-    currentlyVisiting = []
-
-    def oneCourse(courseNumber : 'int') -> bool:
-        if courseNumber not in requirements:
-            return True
-        elif courseNumber in currentlyVisiting:
+    visited = set()
+    def oneCourse(course : int) -> bool:
+        if course in visited:
             return False
-        else:
-            currentlyVisiting.append(courseNumber)
-            for requirement in requirements[courseNumber]:
-                if oneCourse(requirement) == False: return False
+        if requirements[course] == []:
             return True
 
-    return oneCourse(0)        
+        visited.add(course)
+
+        for requirement in requirements[course]:
+            if not oneCourse(requirement): return False
+        visited.remove(course)
+        requirements[course] = []
+        return True
+
+    for course in range(numCourses):
+        if not oneCourse(course): return False
+    return True
+
 
 print(canFinish(5,[[0,1],[0,2],[1,3],[1,4],[3,4]]))
+print(canFinish(20, [[0,10],[3,18],[5,5],[6,11],[11,14],[13,1],[15,1],[17,4]]))
