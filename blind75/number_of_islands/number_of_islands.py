95476d660a8510705acf7eab12ce0d939ed7502d
tyren234 <tyren234@gmail.com>
2022-08-13 23:28:17 +0200

    Number of islands not really working yet, but I'm getting there

--- blind75/number_of_islands/number_of_islands.py
+++ blind75/number_of_islands/number_of_islands.py
@@ -6,4 +6,57 @@ def numIslands(grid: 'list[list[str]]') -> int:
     # ids: 0 1
     #      2 3
     #      4 5
-    def getRoot(node, ):
+
+    rootsList = [i for i in range(rows * columns)]
+
+    def isRoot(nodeId : int, rootsList : 'list[int]'):
+        return rootsList[nodeId] == nodeId
+    
+    def connect(nodeId : int, connectToId : int, rootsList : 'list[int]'):
+        # node's root is now the same as connectTo's root.
+        rootsList[nodeId] = rootsList[connectToId]
+    
+    def connected(nodeId : int, node2Id : int, rootList : 'list[int]') -> bool:
+        return rootList[nodeId] == rootList[node2Id]
+
+    for r in range(rows):
+        for c in range(columns):
+            if grid[r][c] == "0": continue
+
+            id = r * columns + c
+
+            # down check
+            if r != rows - 1 and grid[r + 1][c] == "1" and not connected(id, id + columns, rootsList):
+                if isRoot(id + columns, rootsList): connect(id + columns, id, rootsList)
+                else: connect(rootsList[id], id + columns, rootsList)
+            # right check
+            if c != columns - 1 and grid[r][c + 1] == "1" and not connected(id, id + 1, rootsList):
+                if isRoot(id + 1, rootsList): connect(id + 1, id, rootsList)
+                else: connect(rootsList[id], id + 1, rootsList)
+    
+    # return rootsList
+
+    numberOfIslands = 0
+
+    for r in range(rows):
+        for c in range(columns):
+            if grid[r][c] == "0": continue
+            if isRoot(r * columns + c, rootsList): numberOfIslands += 1
+    
+    return numberOfIslands
+
+# print(numIslands([
+# ["1","1","1","1","0"],
+# ["1","1","0","1","0"],
+# ["1","1","0","0","0"],
+# ["0","0","0","0","0"]]))
+
+# print(numIslands([
+#     ["1","1","1"],
+#     ["0","1","0"],
+#     ["1","1","1"]]))
+
+print(numIslands([
+    ["1","0","1","1","1"],
+    ["1","0","1","0","1"],
+    ["1","1","1","0","1"]])) 
