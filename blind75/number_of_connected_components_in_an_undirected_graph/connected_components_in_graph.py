aa9c8ee8b9aae00a1fcc762a533173b123730bee
tyren234 <39140389+tyren234@users.noreply.github.com>
2022-08-30 23:19:33 +0200

    connected components in an undirected graph
    I couldn't test it so...

--- blind75/number_of_connected_components_in_an_undirected_graph/connected_components_in_graph.py
+++ blind75/number_of_connected_components_in_an_undirected_graph/connected_components_in_graph.py
@@ -1,2 +1,14 @@
 def numberConnected (n : 'int', edges : 'list[list[int]]') -> 'int':
-    pass
+    roots = [i for i in range(n)]
+
+    for one, two in edges:
+        if roots[one] != roots[two]:
+            roots[two] = roots[one]
+    
+    count = 0
+    for node in range(n):
+        if roots[node] == node: count += 1
+    
+    return count
+    
+print(numberConnected(5, [[0,1],[1,2],[3,4]]))
