e140c1f7f5e9f02fe7a081cf238c3a4c1c3d2872
tyren234 <39140389+tyren234@users.noreply.github.com>
2022-09-02 00:02:59 +0200

    end of day 1 insert interval
    I messed up. Wrong lead - will try other way

--- blind75/instert_interval/insert_interval.py
+++ blind75/instert_interval/insert_interval.py
@@ -1,2 +1,20 @@
 def insert(intervals : 'list[list[int]]', newInterval : 'list[int]') -> 'list[list[int]]':
-    pass
+    result = []
+    if intervals[0][0] > newInterval[1]: 
+        result.append(newInterval)
+        result.extend(intervals)
+        return result
+    elif intervals[-1][1] < newInterval[0]:
+        result.extend(intervals)
+        result.append(intervals)
+        return result
+    else:
+        i = len(intervals) - 1
+        while i >= 0:
+            if intervals[i][1] < newInterval[0]:
+                # wstawianie po prawej itego interwalu
+                new = []
+                
+            i -= 1
+
+print(insert([[1,3],[6,9]], [-1,0]))
